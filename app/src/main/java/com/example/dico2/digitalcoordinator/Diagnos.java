f62318668b784c03ee712fba3bc287910edf0a52
Jimmy <jimmy.lundqvist@gmail.com>
2018-05-05 12:58:34 +0200

    Add Diagnos class

--- app/src/main/java/com/example/dico2/digitalcoordinator/Diagnos.java
+++ app/src/main/java/com/example/dico2/digitalcoordinator/Diagnos.java
@@ -1,4 +1,18 @@
 package com.example.dico2.digitalcoordinator;
 
+import java.util.ArrayList;
+
 public class Diagnos {
+    private String diagnos;
+    private int burdening;
+    private ArrayList<Unit> restrictedTo;
+
+    public Diagnos(String diagnos, int burdening) {
+        this.diagnos = diagnos;
+        this.burdening = burdening;
+    }
+
+    public void addRestriction(Unit unit){
+        restrictedTo.add(unit);
+    }
 }
