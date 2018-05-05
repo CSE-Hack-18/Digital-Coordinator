98ad61f1f8f5d06e976d3e5e665bf8e6a57b4c8d
Felix Rosén <felix.m.rosen@gmail.com>
2018-05-05 19:24:47 +0200

    More sp00ks in swiper

--- app/src/main/java/com/example/dico2/digitalcoordinator/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/dico2/digitalcoordinator/ScreenSlidePageFragment.java
@@ -1,10 +1,12 @@
 package com.example.dico2.digitalcoordinator;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ScreenSlidePageFragment extends Fragment {
 
@@ -14,6 +16,8 @@ public class ScreenSlidePageFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                     R.layout.fragment_screen_slide_page, container, false);
 
+
         return rootView;
     }
+
 }
