05ddd448258796ff0d9f4a655e004032d97a166a
Jimmy <jimmy.lundqvist@gmail.com>
2018-05-05 12:23:04 +0200

    Update unit

--- app/src/main/java/com/example/dico2/digitalcoordinator/Unit.java
+++ app/src/main/java/com/example/dico2/digitalcoordinator/Unit.java
@@ -9,4 +9,35 @@ public class Unit {
     private int capacity;
     private String location;
     private String type;
+
+    public Unit(String unit, int capacity, String location, String type) {
+        this.unit = unit;
+        this.capacity = capacity;
+        this.location = location;
+        this.type = type;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+    public ArrayList<Staff> getEmployees() {
+        return employees;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
