f7db5791365a329a61bf791c0fd1e252b41e1a83
Jimmy <jimmy.lundqvist@gmail.com>
2018-05-05 11:40:27 +0200

    Initial project

---
+++ app/src/main/java/com/example/dico2/digitalcoordinator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dico2.digitalcoordinator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
