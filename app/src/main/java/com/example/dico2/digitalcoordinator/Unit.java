f111b2f93b340b37aef11f66daef44958b96a237
DESKTOP-HOTPGCG\John <john.sandell97@gmail.com>
2018-05-05 12:24:30 +0200

    add constructor + variables for Unit

--- app/src/main/java/com/example/dico2/digitalcoordinator/Unit.java
+++ app/src/main/java/com/example/dico2/digitalcoordinator/Unit.java
@@ -1,4 +1,19 @@
 package com.example.dico2.digitalcoordinator;
 
 public class Unit {
+    public enum Discipline {
+        KIRURGI, ORTOPEDI, MEDICIN, GERIATRIK, UROLOGI
+    }
+    int currentWorkload;
+    int capacity;
+    Discipline discipline;
+    int availableBeds;
+
+
+    public Unit(int currentWorkload, int capacity, Discipline discipline, int availableBeds){
+        this.discipline = discipline;
+        this.currentWorkload = currentWorkload;
+        this.capacity = capacity;
+        this.availableBeds = availableBeds;
+    }
 }
