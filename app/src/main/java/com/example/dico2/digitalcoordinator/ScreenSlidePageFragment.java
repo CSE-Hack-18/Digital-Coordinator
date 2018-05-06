7042bb67e2576726f941dd1a8829a213ffd88655
Felix Rosén <felix.m.rosen@gmail.com>
2018-05-06 09:22:19 +0200

    Add admin rights to files

--- app/src/main/java/com/example/dico2/digitalcoordinator/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/dico2/digitalcoordinator/ScreenSlidePageFragment.java
@@ -1,23 +1,17 @@
 package com.example.dico2.digitalcoordinator;
 
-import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class ScreenSlidePageFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(
-                    R.layout.fragment_screen_slide_page, container, false);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate( R.layout.fragment_screen_slide_page, container, false);
 
         return rootView;
     }
-
 }
